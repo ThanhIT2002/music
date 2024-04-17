28a1d60f8d6e0b73fe550d3395cd22bb62d51dd3
ThanhIT2002 <133495724+ThanhIT2002@users.noreply.github.com>
2024-04-17 13:18:39 +0700

    first commit

---
+++ music-list.js
@@ -0,0 +1,54 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+    {
+      name: "ANH SẼ ĐÓN EM",
+      artist: "Trang ft Nguyen",
+      img: "anh_se_don_em",
+      src: "anh_se_don_em"
+    },
+   
+    {
+      name: "EM THÍCH",
+      artist: "Sean ft Lua",
+      img: "em_thich",
+      src: "em_thich"
+    },
+    {
+      name: "Hẹn Gặp Em Dưới Ánh Trăng",
+      artist: "HIEUTHUHAI, HURRYKNG và MANBO",
+      img: "hen_em",
+      src: "hen_em"
+    },
+    {
+      name: "Hư Không",
+      artist: "Kha",
+      img: "hu_khong",
+      src: "hu_khong"
+    },
+    {
+      name: "Miên man",
+      artist: "Minh Huy",
+      img: "mien_man",
+      src: "mien_man"
+    },
+    {
+      name: "Thích Quá Rùi Nà",
+      artist: "TLinh ft Trung Trần",
+      img: "thich_rui",
+      src: "thich_rui"
+    },
+    {
+      name: "YÊU 4",
+      artist: "Rhymastic",
+      img: "yeu4",
+      src: "yeu4"
+    },
+  ];
